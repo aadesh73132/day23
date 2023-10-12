1bebb28e0e8f9359c36237806664ad541c9a0170
aadesh73132 <145355002+aadesh73132@users.noreply.github.com>
2023-10-12 07:44:49 +0530

    Add files via upload

---
+++ day23.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int day=4;
+
+    switch (day)
+    {
+    case 1:
+        printf("monday");
+        break;
+    
+     case 2:
+        printf("tuesday");
+        break;
+    
+     case 3:
+        printf("wednusday");
+        break;
+    
+    
+     case 4:
+        printf("thursday");
+        break;
+    
+    
+     default:
+         printf("no day");
+        break;
+    }
+
+
+    return 0;
+}
